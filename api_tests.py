cf7a8dbfaf2bd78e49cf7869ab87e05406ee90ce
Sacriflces <Jmhservice@gmail.com>
2025-06-28 14:27:42 -0400

    Add humbleclient functionality to save session cookies.

--- api_tests.py
+++ api_tests.py
@@ -1,16 +1,30 @@
 from requests import Request, Session
 from http.cookiejar import MozillaCookieJar
-from humbleclient import HUMBLE_MAIN
+from humbleclient import HUMBLE_MAIN, HumbleClient 
+import os
 #We're going to use the pickle module to save and load the cookies.
-cj = MozillaCookieJar("./cookies.txt")
-with Session() as s:
-    print(f"Cookies before request to humble bundle main page:\n {s.cookies}")
-    print(f"Cookie jar object:")
-    for cookie in iter(cj):
-        print(cookie)
-    r = s.get(HUMBLE_MAIN, cookies=cj)
+
+if not os.path.exists("./cookies"):
+    os.mkdir("./cookies")
+hb = HumbleClient(login="InqindiGaming@gmail.com")
+print(f"Cookies before request to humble bundle main page:")
+hb_cookies = hb.GetSessionCookies()
+for cookie in hb_cookies:
+    print(cookie)
+
+hb.VisitHomePage()
+hb_cookies = hb.GetSessionCookies()
+print(f"Cookies after request to humble bundle main page:")
+for cookie in hb_cookies:
+    print(cookie)
+#with Session() as s:
+#    print(f"Cookies before request to humble bundle main page:\n {s.cookies}")
+#    print(f"Cookie jar object:")
+#    for cookie in iter(cj):
+#        print(cookie)
+#    r = s.get(HUMBLE_MAIN, cookies=cj)
 #    print(r.text) 
-    print(f"Cookies after request to humble bundle main page: \n {s.cookies}")
-    print(f"Cookie jar object:")
-    for cookie in iter(cj):
-        print(cookie)
+#    print(f"Cookies after request to humble bundle main page: \n {s.cookies}")
+#    print(f"Cookie jar object:")
+#    for cookie in iter(cj):
+#        print(cookie)
--- humbleclient.py
+++ humbleclient.py
@@ -1,4 +1,7 @@
 from abc import ABC, abstractmethod
+from requests import Session
+import pickle
+from os.path import exists
 
 HUMBLE_MAIN = "https://www.humblebundle.com/"
 
@@ -9,32 +12,67 @@ class GameKeyClient(ABC):
         pass
 
     @abstractmethod
-    def GetGamesInfo():
+    def GetGamesInfo(self):
         pass
 
     @abstractmethod
-    def ChooseContent(gamekey, identifiers):
+    def ChooseContent(self, gamekey, identifiers):
         pass
 
     @abstractmethod
-    def RedeemKey(keytype, gamekey, keyindex):
+    def RedeemKey(self, keytype, gamekey, keyindex):
         pass
 
 class HumbleClient(GameKeyClient):
     
-    def __init__(session):
-        if not session:
-            raise Exception("session cannot be null")
-        self.__session = session
+    def __init__(self, login=None, password=None):
+        self.__session = Session()
+        self.__login = login
+        self.__password = password
+        self.__loggedIn = False
+        self.__LoadCookies()
 
     def Login(self, login, password):
         pass
 
-    def GetGamesInfo():
+    def GetGamesInfo(self):
         pass
 
-    def ChooseContent(gamekey, identifiers):
+    def ChooseContent(self, gamekey, identifiers):
         pass
 
-    def RedeemKey(keytype, gamekey, keyindex=0):
+    def RedeemKey(self, keytype, gamekey, keyindex=0):
         pass
+
+    def VisitHomePage(self):
+        if self.__session:
+            self.__session.get(HUMBLE_MAIN)
+
+    def GetSessionCookies(self):
+        if self.__session:
+            return self.__session.cookies.copy()
+
+    def __LoadCookies(self):
+        if not self.__session or not self.__login:
+            return
+        cookies_file = f"./cookies/hb_{self.__login.lower()}_cookies.txt"
+        if not exists(cookies_file):
+            return
+        with open(cookies_file, "r+b") as f:
+            cj = pickle.load(f)
+            cj.clear_expired_cookies()
+            self.__session.cookies = cj
+    
+    def __del__(self):
+        if self.__session:
+            try:
+                if self.__login:
+                    cookies_file = f"./cookies/hb_{self.__login.lower()}_cookies.txt"
+                    with open(cookies_file, "w+b") as f:
+                        pickle.dump(self.__session.cookies, f)
+            except Exception as ex:
+                print(f"Exception of type {type(ex)}: {ex}")
+            finally:
+                self.__session.close()
+
+
