08a27322432e90d1b47e3eb1dcd04be9a52fe091
jmservic <jmhservice@gmail.com>
2025-10-17 16:43:59 -0400

    Updated http_utils to change the date format of the expires key to a format accepted by the requests module.

--- src/clients/http_utils.py
+++ src/clients/http_utils.py
@@ -1,4 +1,5 @@
 import http.cookies
+from datetime import datetime
 
 def SetCookieHeaderToMorsels(headers):
     #headers is a list of (header, value) tuples like that returned from HTTPResponse.getheaders()
@@ -6,4 +7,23 @@ def SetCookieHeaderToMorsels(headers):
     for (name, value) in headers:
         if(name == "Set-Cookie"):
             morsels.load(rawdata=value)
+            #print(morsels.keys())
+            #print(value)
+    for morsel in morsels.values():
+        if "expires" in morsel.keys():
+            #print("before", morsel["expires"])
+            morsel["expires"] = createRequestDateTimeStr(morsel["expires"])
+            #print("after", morsel["expires"])
     return morsels
+
+def createRequestDateTimeStr(date_str):
+    try:
+        new_date_str = datetime.strptime(date_str, "%a, %d %b %Y %H:%M:%S GMT").strftime("%a, %d-%b-%Y %H:%M:%S GMT")
+        return new_date_str
+    except:
+        try:
+            new_date_str = datetime.strptime(date_str, "%a, %d-%b-%y %H:%M:%S GMT").strftime("%a, %d-%b-%Y %H:%M:%S GMT")
+            return new_date_str
+        except:
+            return date_str
+        
